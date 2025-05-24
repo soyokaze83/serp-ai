93154401c12836d4b3f7414b3b183980394f0a30
soyokaze83 <vinjupt@gmail.com>
2025-05-24 16:19:35 +0700

    fix: remove rule entirely

--- frontend/eslint.config.mjs
+++ frontend/eslint.config.mjs
@@ -13,11 +13,7 @@ const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
   {
     rules: {
-      '@typescript-eslint/no-unused-vars': ['error', {
-        'argsIgnorePattern': '^_',
-        'varsIgnorePattern': '^(index|_)$',
-        'caughtErrorsIgnorePattern': '^_',
-      }],
+      '@typescript-eslint/no-unused-vars': 'off',
     },
   },
 ];
